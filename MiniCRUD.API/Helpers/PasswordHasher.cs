fdc8ff17328b75ef8dc49a4724a25d353338f985
nick4real <niko.wsnh@gmail.com>
2025-10-23 20:57:29 +0200

    Create PasswordHasher.cs

---
+++ MiniCRUD.API/Helpers/PasswordHasher.cs
@@ -0,0 +1,15 @@
+﻿namespace MiniCRUD.API.Helpers
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            // Simple hash for demonstration purposes only. Use a secure hashing algorithm in production.
+            return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            return HashPassword(password) == hashedPassword;
+        }
+    }
+}
