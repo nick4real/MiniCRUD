8c0e1a6613e3ec57e2a9cc160d976af81f6c6553
nick4real <niko.wsnh@gmail.com>
2025-10-23 20:55:21 +0200

    Aspire startup update

--- MiniCRUD.AppHost/AppHost.cs
+++ MiniCRUD.AppHost/AppHost.cs
@@ -1,7 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.MiniCRUD_API>("minicrud-api");
+var minicrud_api = builder.AddProject<Projects.MiniCRUD_API>("minicrud-api");
 
-builder.AddProject<Projects.MiniCRUD_WebApp>("minicrud-webapp");
+var minicrud_webapp = builder.AddProject<Projects.MiniCRUD_WebApp>("minicrud-webapp")
+    .WithReference(minicrud_api);
 
 builder.Build().Run();
