79bca5e3de6e3f5e42ed23c32bd90f6f58109d98
nick4real <niko.wsnh@gmail.com>
2025-10-24 01:53:02 +0200

    Gateway configuring

--- MiniCRUD.AppHost/AppHost.cs
+++ MiniCRUD.AppHost/AppHost.cs
@@ -5,4 +5,8 @@ var minicrud_api = builder.AddProject<Projects.MiniCRUD_API>("minicrud-api");
 var minicrud_webapp = builder.AddProject<Projects.MiniCRUD_WebApp>("minicrud-webapp")
     .WithReference(minicrud_api);
 
+var minicrud_gateway = builder.AddProject<Projects.MiniCRUD_Gateway>("minicrud-gateway")
+    .WithReference(minicrud_api)
+    .WithReference(minicrud_webapp);
+
 builder.Build().Run();
---
+++ MiniCRUD.Gateway/Program.cs
@@ -0,0 +1,44 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+void ConfigureReverseProxy()
+{
+    var reverseProxyClusters = builder.Configuration.GetSection("ReverseProxy:Clusters");
+
+    foreach (var cluster in reverseProxyClusters.GetChildren())
+    {
+        var clusterName = cluster.Key.Replace("-cluster", string.Empty);
+
+        var tmp2 = builder.Configuration.GetSection($"ReverseProxy:Clusters:{cluster.Key}:Destinations:destination-1:Address");
+        var destinationUrl2 = Environment.GetEnvironmentVariable($"services__{clusterName}__http__0");
+        tmp2.Value = destinationUrl2;
+    }
+}
+
+ConfigureReverseProxy();
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapReverseProxy();
+
+app.Run();
